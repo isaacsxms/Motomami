75bcf3097f78ebcab509c9290b57ba3173c25dcc
isaacsxms <isaacsxms@gmail.com>
2024-04-24 18:54:57 +0200

    kjjioi

--- src/main/java/com/dam/tfg/MotoMammiApplicationIDS/services/implement/ProcessImpl.java
+++ src/main/java/com/dam/tfg/MotoMammiApplicationIDS/services/implement/ProcessImpl.java
@@ -5,9 +5,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.dam.tfg.MotoMammiApplicationIDS.Utils.HibernateUtil;
@@ -16,12 +18,19 @@ import com.dam.tfg.MotoMammiApplicationIDS.services.ProcessService;
 
 @Component
 public class ProcessImpl implements ProcessService{
+    @Value("${path.file.customer.name}")
+    private String customersNameFile;
+    @Value("${path.file.vehicles.name}")
+    private String vehiclesNameFile;
+    @Value("${path.file.parts.name}")
+    private String partsNameFile;
+
     @Override
     public void readFileInfo(String p_source, String p_prov, Date p_date) {
         HibernateUtil.buildSessionFactory();
         HibernateUtil.openSession();
 
-        String resource = "src/main/resources/in";
+        String resource = "src/main/resources/in/";
         
         // Check if active and date in between initialized and end provider date.
         String getActiveSources = "FROM MM_PROVIDERS WHERE active = TRUE AND COALESCE(:p_date, CURRENT_DATE()) BETWEEN initializeDate AND COALESCE(endDate, '2099-12-30')";
@@ -36,37 +45,39 @@ public class ProcessImpl implements ProcessService{
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String formattedDate = dateFormat.format(p_date);
-
+        String findCustomersFile = null;
+        String findVehiclesFile = null;
+        String findPartsFile = null;
+        ArrayList<String> files = new ArrayList<>();
 
         for (ProviderDTO activeSource : activeSources) {
             providerCode = activeSource.getProviderCode();
-            String findFile = "MM_invoices_" + providerCode + "_" + formattedDate + ".dat";
-            System.out.println("File path: " + findFile);
+                findCustomersFile = resource + customersNameFile + providerCode + "_" + formattedDate + ".dat";
+                findVehiclesFile = resource + vehiclesNameFile + providerCode + "_" + formattedDate + ".dat";
+                findPartsFile = resource + partsNameFile + providerCode + "_" + formattedDate + ".dat";
 
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(findFile));
-                int line;
+                files.add(findCustomersFile);
+                //files.add(findVehiclesFile);
+                //files.add(findPartsFile);
+
+            for (String file : files) {
                 try {
-                    while ((line = br.read()) != -1) {
-                        System.out.println("Reading: " + line);
+                    BufferedReader br = new BufferedReader(new FileReader(file));
+                    String line;
+                    try {
+                        br.readLine(); // skip first line (column names)
+                        while ((line = br.readLine()) != null) {
+                            System.out.println("Reading file " + file + ": \n" + line + "\n");
+                            
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
+                } catch (FileNotFoundException e) {
+                    System.err.println("No se encontro el fichero! " + file);
                 }
-            } catch (FileNotFoundException e) {
-                System.err.println("No se encontro el fichero! " + findFile);
             }
-        }
-
-        
-
-        // need COD_PROV, Source and p_date to read from file
-        
-
-
-        
-        // Process the results
-        
+        }        
         HibernateUtil.closeSessionFactory();
     }
 }
